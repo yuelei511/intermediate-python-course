a39e73660ae928970f513ba52d65c8cef7cfa1f3
yuelei <leiyue0611@gmail.com>
2020-12-27 19:39:38 +0800

    User input on number of players, number of dice, size of dice. The one rolled the highest sum will win.

--- dice_roller.py
+++ dice_roller.py
@@ -1,15 +1,19 @@
 import random
 
 def main():
-  player = 2
+  player = int(input('Enter the number players: '))
+  dice_num = int(input('Enter the number of dice to be rolled for each player: '))
+  dice_size = int(input('Enter the size of the dice used for this game: '))
   player_rolls = []
-  max = 6
+  max = float('inf')
   winner = 0
   for i in range(0, player):
-    roll = random.randint(1, 6)
-    player_rolls.append(roll)
-    dice_sum += roll
-    print(f'player{i+1} rolled {roll}')
+    roll_sum = 0
+    for j in range(0, dice_num):
+      roll = random.randint(1, 6)
+      roll_sum += roll
+    player_rolls.append(roll_sum)
+    print(f'player{i+1} rolled  a sum of {roll_sum}')
     if player_rolls[i] <= max:
       max = player_rolls[i]
       winner = i+1
